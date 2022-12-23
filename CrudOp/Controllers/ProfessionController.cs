ebb1afaba25ba41068e419e3a5bdb115d89428f6
chahal48 <sachin.chahal48@gmail.com>
2022-12-23 11:59:12 +0530

    Crud Core 6.0 without EF

---
+++ CrudOp/Controllers/ProfessionController.cs
@@ -0,0 +1,81 @@
+﻿using CrudOp.Models;
+using CrudOp.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrudOp.Controllers
+{
+    public class ProfessionController : Controller
+    {
+        private readonly ProfessionRepository _professionRepo;
+        public ProfessionController(ProfessionRepository professionRepo)
+        {
+            _professionRepo = professionRepo;
+        }
+
+        public IActionResult Index()
+        {
+            ModelState.Clear();
+
+            return View(_professionRepo.GetAllProfessions().OrderByDescending(Profession => Profession.ProfessionID));
+        }
+        public IActionResult AddProfession()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddProfession(ProfessionModel professionModel)
+        {
+            if (ModelState.IsValid)
+            {
+                if (_professionRepo.AddProfession(professionModel))
+                {
+                    return RedirectToAction("Index", "Profession");
+                }
+            }
+            return View();
+        }
+        public IActionResult EditProfessionDetails(int id)
+        {
+            return View(_professionRepo.GetAllProfessions().Find(prof => prof.ProfessionID == id));
+        }
+
+        [HttpPost]
+        public IActionResult EditProfessionDetails(int id, ProfessionModel professionModel)
+        {
+            if (ModelState.IsValid)
+            {
+                if (_professionRepo.UpdateProfession(professionModel))
+                {
+                    return RedirectToAction("Index", "Profession");
+                }
+            }
+            return View(professionModel);
+        }
+        public IActionResult DeleteProfession(int id)
+        {
+            if (_professionRepo.DeleteProfession(id))
+            {
+                ViewBag.AlertMsg = "Profession details deleted successfully";
+            }
+            return RedirectToAction("Index","Profession");
+        }
+        [AcceptVerbs("Get", "Post")]
+        public IActionResult IsProfessionAvailable([Bind(Prefix = "Profession")] string Profession, string initialProfession)
+        {
+            if (initialProfession != "" && initialProfession != null)
+            {
+                if (Profession.ToLower() == initialProfession.ToLower())
+                {
+                    return new JsonResult(true);
+                }
+            }
+            var result = _professionRepo.GetAllProfessions().FirstOrDefault(a => a.Profession.ToLower() == Profession.ToLower());
+            if (result == null)
+            {
+                return new JsonResult(true);
+            }
+            return new JsonResult(false);
+        }
+    }
+}
--- CrudOp/Models/ProfessionModel.cs
+++ CrudOp/Models/ProfessionModel.cs
@@ -6,9 +6,6 @@ namespace CrudOp.Models
 {
     public class ProfessionModel
     {
-        [DisplayName("S.No.")]
-        public int SerialNo { get; set; }
-
         [DisplayName("Profession ID")]
         [Required(ErrorMessage = "Id is mandatory!!")]
         public int ProfessionID { get; set; }
@@ -17,13 +14,13 @@ namespace CrudOp.Models
         [Required(ErrorMessage = "Profession name is mandatory!!")]
         [StringLength(30, MinimumLength = 3, ErrorMessage = "Profession name character length should be in between 3 to 30.")]
         [RegularExpression("^[A-Za-z -]*$", ErrorMessage = "Sorry, only letters (a-z) are allowed.")]
-        //[Remote("IsProfessionAvailable", "Profession", HttpMethod = "Post", ErrorMessage = "Profession already exist.", AdditionalFields = "initialProfession")]
+        [Remote("IsProfessionAvailable", "Profession", ErrorMessage = "Profession is already in use.", AdditionalFields = "initialProfession")]
         public string Profession { get; set; }
 
         [DisplayName("Description")]
         [StringLength(200, MinimumLength = 3, ErrorMessage = "Description character length should be in between 3 to 200.")]
-        [RegularExpression("^[a-zA-Z0-9'@&#.\\s]*$", ErrorMessage = "Sorry, only letters (a-z), numbers (0-9), and periods ('@&#.) are allowed.")]
-        public string Description { get; set; }
+        [RegularExpression("^[a-zA-Z0-9',/@&#.\\s]*$", ErrorMessage = "Sorry, only letters (a-z), numbers (0-9), and periods ('@&#.) are allowed.")]
+        public string? Description { get; set; }
 
         public string initialProfession
         {
